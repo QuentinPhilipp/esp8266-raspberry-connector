b59896e523021d6fa9254d7e093113b99df1e284
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-08-26 15:53:03 +0200

    Web server init

---
+++ ESP8266/sensorCode/sensorCode.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>   // Include the WebServer library
+
+char* device_name = "ROOM_1";
+
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+void handleRoot();
+void handshake();
+void handleNotFound();
+
+const char* ssid     = "Livebox-D1F5";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "752F52EA9A0A06597356745910";     // The password of the Wi-Fi network
+
+void setup() {
+  Serial.begin(9600);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+
+  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
+  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+  server.on("/check", handshake);               // Call the 'handshake' function
+
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+
+}
+
+void loop(void){
+  server.handleClient();                    // Listen for HTTP requests from clients
+}
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello world!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void handshake(){
+  String data = "{'status':'OK','device_name':'";
+  data = data +device_name;
+  data = data + "'}";
+
+  server.send(200, "json/application", data);
+}
